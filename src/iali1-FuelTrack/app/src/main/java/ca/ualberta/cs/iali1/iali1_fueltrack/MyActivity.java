26bfe6c07294248797506267f51f8e79df51adcc
Imran Ali <iali1@cs.ualberta.ca>
2016-01-27 13:22:59 -0700

    day1

--- src/iali1-FuelTrack/app/src/main/java/ca/ualberta/cs/iali1/iali1_fueltrack/MyActivity.java
+++ src/iali1-FuelTrack/app/src/main/java/ca/ualberta/cs/iali1/iali1_fueltrack/MyActivity.java
@@ -1,37 +1,84 @@
 package ca.ualberta.cs.iali1.iali1_fueltrack;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MyActivity extends ActionBarActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MyActivity extends Activity {
+    private static final String FILENAME = "file.sav";
+    private EditText bodyText;
+    private ListView oldEntryLogs;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.main);
+
+        Button addButton = (Button) findViewById(R.id.add);
+        oldEntryLogs = (ListView) findViewById(R.id.oldLogList);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        //String[] tweets = loadFromFile();
+        //loadFromFile();
     }
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            //Gson gson = new Gson();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Took from https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html 01-19 2016
+            //Type listType = new TypeToken<ArrayList<NormalTweet>>() {}.getType();
+            //tweets = gson.fromJson(in, listType);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            //tweets = new ArrayList<Tweet>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    0);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+            //Gson gson = new Gson();
+            //gson.toJson(tweets, out);
+            out.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
 }
